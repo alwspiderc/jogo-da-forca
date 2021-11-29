882a2efb64cc68ca1aff9d404c0f0344074bd485
Ester <alwesterc@gmail.com>
2021-11-29 08:28:29 -0300

    .

---
+++ src/br/com/ester/jogoDaForca/excecao/PerderException.java
@@ -0,0 +1,10 @@
+package br.com.ester.jogoDaForca.excecao;
+
+public class PerderException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- src/br/com/ester/jogoDaForca/modelo/Area.java
+++
@@ -1,6 +0,0 @@
-package br.com.ester.jogoDaForca.modelo;
-
-public class Area {
-
-	
-}
